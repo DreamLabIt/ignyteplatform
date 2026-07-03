0ce1169f8f82915ff38ba7a0c469f328e6af0b4f
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-07-03 17:51:54 +0600

    layout and home page structure for improved styling and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Footer from "@/components/(Common)/Footer";
-import Navbar from "@/components/(Common)/Navbar";
-
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-background px-6 text-center">
-        <div className="space-y-4">
-          <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight text-red-500">
+      <main className="flex-1  flex-col items-center justify-center bg-background px-6 text-center">
+        <div className="space-y-4 ">
+          <h1 className="text-5xl md:text-7xl my-20 font-extrabold tracking-tight text-red-500">
             IgnytePlatform
           </h1>
         </div>
