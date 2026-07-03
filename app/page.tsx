8908b1427e87892af65be5feb801b21d57c49326
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-07-03 11:20:48 +0600

    Initial commit with Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background px-6 text-center">
+      <div className="space-y-4">
+        <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight text-red-500">
+          IgnytePlatform
+        </h1>
+      </div>
+    </main>
+  );
+}
