652a78e4e64c8bb8057c26630eaf4f097c6172a6
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-07-06 18:40:54 +0600

    Add FedRamp component with dynamic content and background image for enhanced compliance presentation

--- app/certification/page.tsx
+++ app/certification/page.tsx
@@ -1,10 +1,12 @@
 import CardSection from "@/components/(Certification)/CardSection";
 import CloudService from "@/components/(Certification)/CloudService";
 import CustomerStories from "@/components/(Certification)/CustomerStoriesSection";
+import FedRamp from "@/components/(Certification)/FedRamp,";
 
 export default function Certification() {
   return (
     <>
+      <FedRamp />
       <CloudService />
       <CustomerStories />
       <CardSection />
