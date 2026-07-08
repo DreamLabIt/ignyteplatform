22e076062c083abdade95d4fd85ff3d805ea6c3e
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-07-08 18:09:17 +0600

    Refactor Hero component for improved layout and styling; adjust padding, typography, and background properties for better visual appeal

--- components/(Compliance)/Hero.tsx
+++ components/(Compliance)/Hero.tsx
@@ -41,8 +41,7 @@ const featureData: FeatureItem[] = [
 export default function Hero() {
     return (
         <div
-
-            className="w-full relative pt-32 pb-8 overflow-hidden"
+            className="w-full relative pt-70 pb-26 overflow-hidden"
             style={{
                 backgroundImage: `linear-gradient(180deg, rgba(241, 241, 241, 0.56) 51.58%, #F1F1F1 77.61%), url(${HeroBg.src})`,
                 backgroundSize: "cover",
@@ -50,15 +49,15 @@ export default function Hero() {
                 backgroundRepeat: "no-repeat",
             }}
         >
-            <section className="w-full relative z-10 px-6">
+            <section className="w-full relative z-10 px-6 ">
                 <div className="mx-auto w-full flex flex-col items-center">
 
                     <div className="w-full flex flex-col items-center text-center">
-                        <h1 className="font-Inter font-bold text-[36px] sm:text-[48px] md:text-[56px] leading-[120%] text-[#3B7CB3] tracking-tight mb-4">
+                        <h1 className="font-Inter font-semibold text-[36px] sm:text-[48px] md:text-[64px] leading-[130%] text-[#3B7CB3] tracking-tight mb-8">
                             Get CMMC Certified <span className="text-[#1E374F]">in Weeks, <br className="hidden sm:inline" /> Not Months</span>
                         </h1>
 
-                        <p className="font-Inter font-medium text-[15px] sm:text-[16px] md:text-[17px] leading-[150%] text-[#4A607A] max-w-162.5 mb-6">
+                        <p className="font-Inter font-medium text-[15px] sm:text-[16px] md:text-[20px] leading-[130%] text-[#4A607A] max-w-212.5 mb-8">
                             Ignyte gives you the expert support and automated platform you need to accelerate the CMMC certification process
                         </p>
 
@@ -67,36 +66,32 @@ export default function Hero() {
                         </div>
                     </div>
 
-                    <div className="w-full max-w-275 aspect-16/9.5 relative rounded-2xl overflow-hidden">
-                        <div className="w-full h-full relative rounded-xl overflow-hidden">
-                            <Image
-                                src={CmmcDashboard}
-                                alt="Ignyte CMMC Compliance Dashboard Showcase"
-                                fill
-                                priority
-                                sizes="(max-w: 1200px) 100vw, 1100px"
-                                className="object-cover object-top"
-                            />
-                        </div>
+                    <div className="w-full max-w-[1620px] h-auto aspect-1620/753 relative rounded-2xl overflow-hidden mx-auto mt-24">
+                        <Image
+                            src={CmmcDashboard}
+                            alt="Ignyte CMMC Compliance Dashboard Showcase"
+                            fill
+                            priority
+                            sizes="(max-width: 1620px) 100vw, 1620px"
+                            className="object-cover object-top"
+                        />
                     </div>
 
                 </div>
 
-                <div className="absolute bottom-0 left-0 right-0 h-[20%] bg-linear-to-t from-[#F3F9FE] to-transparent pointer-events-none z-10" />
+                <div className="absolute -bottom-2 left-0 right-0 h-[50%] bg-linear-to-t from-[#ebebec] to-transparent pointer-events-none z-10" />
             </section>
 
+            <section className="w-full relative pt-38 z-10 px-6">
+                <div className="absolute -top-2 left-0 right-0 h-[20%] bg-linear-to-b from-[#ebebec] to-transparent pointer-events-none z-10" />
 
+                <div className="max-w-[1640px] mx-auto w-full flex flex-col items-center">
 
-            <section className="w-full relative pt-20 z-10 px-6">
-                <div className="absolute top-0 left-0 right-0 h-[10%] bg-linear-to-b from-[#F3F9FE] to-transparent pointer-events-none z-10" />
-
-                <div className="max-w-310 mx-auto w-full flex flex-col items-center">
-
-                    <div className="w-full text-center mb-12 md:mb-16 max-w-240">
-                        <h2 className="font-Inter font-bold text-[26px] sm:text-[32px] md:text-[38px] leading-[130%] text-[#1E374F] mb-6 tracking-tight">
+                    <div className="w-full text-center mb-12 md:mb-23 max-w-250">
+                        <h2 className="font-Inter font-semibold text-[26px] sm:text-[32px] md:text-[40px] leading-[130%] text-[#1E374F] mb-6 tracking-tight">
                             <span className="text-[#3B7CB3]">Don’t Let Compliance Delays</span> Put Your <br className="hidden sm:inline" /> Contracts at Risk
                         </h2>
-                        <div className="flex flex-col gap-4 font-Inter font-normal text-[14px] sm:text-[15px] md:text-[16px] leading-[160%] text-[#4A607A] max-w-205 mx-auto">
+                        <div className="flex flex-col gap-6 font-Inter font-medium text-[14px] sm:text-[15px] md:text-[18px] leading-[114%] text-[#4A607A] max-w-265 mx-auto">
                             <p>
                                 Achieving CMMC compliance at any level requires businesses to understand hundreds of rules and assemble documentation, credentials, logs, documented policies, and more in an incredibly short timeframe.
                             </p>
@@ -106,14 +101,14 @@ export default function Hero() {
                         </div>
                     </div>
 
-                    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 mb-12">
+                    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 mb-10 px-6">
                         {featureData.map((feature, index) => (
                             <div
                                 key={index}
-                                className="w-full bg-[#E3EFFC]/90 backdrop-blur-md border border-white/10 rounded-2xl p-6 sm:p-8 flex flex-col items-start transition-all duration-300 hover:bg-[#E3EFFC]/70 shadow-xs"
+                                className="w-full bg-[#92B5D740] backdrop-blur-md border border-[#E2E2E2] rounded-2xl p-8 py-6 flex flex-col items-start transition-all duration-300 hover:bg-[#E3EFFC]"
                             >
-                                <div className="w-12 h-12 sm:w-14 sm:h-14 rounded-xl flex items-center justify-center mb-6 ">
-                                    <div className="relative w-10 h-10 sm:w-12 sm:h-12">
+                                <div className="flex items-center justify-center mb-10">
+                                    <div className="relative w-21.5 h-21.5 ">
                                         <Image
                                             src={feature.icon}
                                             alt={feature.title}
@@ -123,10 +118,10 @@ export default function Hero() {
                                     </div>
                                 </div>
 
-                                <h3 className="font-Inter font-bold text-[18px] sm:text-[20px] lg:text-[22px] leading-[135%] text-[#1E374F] mb-3">
+                                <h3 className="font-Inter font-semibold text-[18px] sm:text-[20px] lg:text-[32px] leading-[135%] text-[#1E374F] mb-4">
                                     {feature.title}
                                 </h3>
-                                <p className="font-Inter font-normal text-[14px] sm:text-[15px] leading-[150%] text-[#4A607A]">
+                                <p className="font-Inter font-semibold text-[14px] sm:text-[20px] leading-[120%] text-[#1E374F]">
                                     {feature.description}
                                 </p>
                             </div>
