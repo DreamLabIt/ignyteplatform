9367a851522ea3792a06806665a1ee1a233f783f
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-07-14 12:58:46 +0600

    Update CloudService component for improved layout, spacing, and styling consistency

--- components/(Certification)/CloudService.tsx
+++ components/(Certification)/CloudService.tsx
@@ -49,9 +49,9 @@ export default function CloudService() {
 
     return (
         <section
-            className="w-full pt-25  pb-72 px-6 relative flex flex-col justify-center items-center"
+            className="w-full pt-25  pb-74 px-6 relative flex flex-col justify-center items-center"
             style={{
-                backgroundImage: `linear-gradient(to bottom, rgba(20, 35, 55, 0.21), rgba(26, 43, 67, 0.21)), url(${SectionBg.src})`,
+                backgroundImage: `linear-gradient(0deg, rgba(24, 45, 67, 0.1) 62.68%, #182D43 100%), url(${SectionBg.src})`,
                 backgroundSize: "100% 100%",
                 backgroundPosition: "bottom",
                 backgroundRepeat: "no-repeat",
@@ -60,7 +60,7 @@ export default function CloudService() {
             <div className="max-w-[1620px] mx-auto w-full">
                 <div className="flex flex-col lg:flex-row lg:items-end lg:justify-between gap-8 mb-12">
                     <div className="">
-                        <h2 className="text-[28px] sm:text-[34px] md:text-[40px] font-semibold font-Inter text-[#ECEBEC] leading-[125%] tracking-[0%] w-full max-w-[890px] mb-8.5">
+                        <h2 className="text-[28px] sm:text-[34px] md:text-[40px] font-semibold font-Inter text-[#ECEBEC] leading-[125%] tracking-[0%] w-full max-w-222.5 mb-8.5">
                             See How Ignyte Helps Cloud Service Providers Achieve ATO Rapidly and Efficiently
                         </h2>
                         <p className="font-Inter font-medium text-[16px] sm:text-[18px] md:text-[20px] leading-[120%] tracking-[-1.4%] text-[#ECEBEC]/80 max-w-187.75">
@@ -69,7 +69,7 @@ export default function CloudService() {
                     </div>
 
                     <div className="shrink-0">
-                        <PrimaryButton href="/get-started" text=" Get Started" minWidth="min-w-[185px]"/>
+                        <PrimaryButton href="/get-started" text=" Get Started" minWidth="min-w-[185px]" />
                     </div>
                 </div>
 
@@ -77,13 +77,13 @@ export default function CloudService() {
                     {cardsData.map((card) => (
                         <div
                             key={card.id}
-                            className="w-full lg:max-w-128.75 min-h-122 backdrop-blur-sm rounded-[30px] p-6 flex flex-col justify-between shadow-xl transition-all duration-300 hover:transform hover:-translate-y-2 border border-white/20 pb-10"
+                            className="w-full lg:max-w-128.75 min-h-122 backdrop-blur-sm rounded-[30px] p-6 flex flex-col justify-between shadow-xl transition-all duration-300 hover:transform hover:-translate-y-2 border border-white/20 pb-7"
                             style={{
                                 background: `linear-gradient(204.15deg, rgba(146, 181, 215, 0) 7.03%, rgba(146, 181, 215, 0.4) 94.58%), #EAF0F6`
                             }}
                         >
-                            <div>
-                                <div className="w-[86px] h-[86px] flex items-center justify-center mb-6 relative overflow-hidden rounded-sm">
+                            <div className="relative">
+                                <div className="w-21.5 h-21.5 flex items-center justify-center mb-6.5 relative overflow-hidden rounded-sm ">
                                     <Image
                                         src={card.iconSrc}
                                         alt={`${card.title} icon`}
@@ -93,22 +93,22 @@ export default function CloudService() {
                                     />
                                 </div>
 
-                                <h3 className="font-Inter font-semibold text-[22px] sm:text-[24px] leading-[108%] text-[#1E374F] mb-14">
+                                <h3 className="font-Inter font-semibold text-[22px] sm:text-[24px] tracking-[0.8%] leading-[120%] text-[#1E374F] mb-14">
                                     {card.title}
                                 </h3>
 
-                                <p className="font-Inter font-normal text-[15px] sm:text-[16px] leading-[150%] text-[#1E374F]/80 mb-12">
+                                <p className="font-Inter font-normal text-[15px] sm:text-[18px] leading-[125%] tracking-[-0.9%] text-[#1E374F] max-w-106.25 mb-4.5">
                                     {card.description}
                                 </p>
                             </div>
 
-                            <div className="w-full h-65 relative rounded-[25px] overflow-hidden ">
+                            <div className="w-full h-70 relative rounded-[25px] overflow-hidden ">
                                 <Image
                                     src={card.graphicSrc}
                                     alt={card.graphicAlt}
                                     fill
                                     sizes="(max-w: 768px) 100vw, 465px"
-                                    className="object-cover"
+                                    className="object-cover border-5 border-[#FFFFFF5C] rounded-[25px]"
                                 />
                             </div>
                         </div>
